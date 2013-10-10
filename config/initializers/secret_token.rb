9a4b79104a8149e696adeaab18861e16e97c6139
YojiBiomehanika <burchenya@gmail.com>
2013-10-10 19:13:59 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cofftaboo::Application.config.secret_key_base = '7d79fd9d0ec0deddc43a14912bd9e33cdb0add406df5755e97d99c9336dbff9bb3948f72dc8bd53ffdc8df69ad5fd87339b9c41642bf8016f50798da95808b95'
